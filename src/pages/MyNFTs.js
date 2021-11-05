0d5b3277991fdffb6fe1ce5734c01bd08e14b5c9
Matthew Mulford <mhmulford0@gmail.com>
2021-11-05 12:43:07 -0500

    update parking from testing

--- src/pages/MyNFTs.js
+++ src/pages/MyNFTs.js
@@ -252,7 +252,7 @@ const MyNFTs = () => {
                       name="level"
                     >
                       <option value="0">Choose Fee</option>
-                      <option value="0.5">$0.50</option>
+                      <option value="10">$10</option>
                       <option value="25">$25</option>
                       <option value="50">$50</option>
                     </select>
